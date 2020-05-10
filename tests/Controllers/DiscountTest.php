3ff35072be267778c041020b81cd707c1f6dc736
Dananjaya <dananjaya01@gmail.com>
2020-05-10 09:58:05 +0530

    unit testing

--- tests/Controllers/DiscountTest.php
+++ tests/Controllers/DiscountTest.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 use App\Entity\XtBook;
 
-class DefaultControllerTest extends WebTestCase
+class DiscountTest extends WebTestCase
 {
     private $em;
 
@@ -23,48 +23,7 @@ class DefaultControllerTest extends WebTestCase
         ;
     }
 
-    public function testSiteTitleAvailable()
-    {
-        $client = static::createClient();
-
-        $client->request('GET', '/');
-
-        $this->assertSelectorTextContains('html .navbar strong', 'Book Store');
-    }
-    
-    public function testCategoriesAvailable()
-    {
-        $client = static::createClient();
-
-        $client->request('GET', '/');
-
-        $this->assertSelectorTextContains('html .list-group .list-group-item-action[href="all"]', 'All Books');
-        $this->assertSelectorTextContains('html .list-group .list-group-item-action[href="1"]', 'Children');
-        $this->assertSelectorTextContains('html .list-group .list-group-item-action[href="2"]', 'Fiction');
-    }
-
-    public function testChildrenCategory()
-    {
-        $client = static::createClient();
-
-        $client->request('GET', '/1');
-
-        $this->assertSelectorTextContains('html .books:first-child', 'Fundamentals of Wavelets');
-        $this->assertSelectorTextContains('html .books:first-child', 'LKR1,000.00');
-        $this->assertSelectorTextContains('html .books:first-child .btn-add-to-cart', 'Add to Cart');
-    }
-
-    public function testFictionCategory()
-    {
-        $client = static::createClient();
-
-        $client->request('GET', '/2');
-
-        $this->assertSelectorTextContains('html .books:first-child', 'Data Smart');
-        $this->assertSelectorTextContains('html .books:first-child', 'LKR500.00/=');
-        $this->assertSelectorTextContains('html .books:first-child .btn-add-to-cart', 'Add to Cart');
-    }
-
+    /* Add to cart */
     public function testAddToCart()
     {
         $client = static::createClient();
@@ -81,6 +40,7 @@ class DefaultControllerTest extends WebTestCase
         );
     }
 
+    /* Remove from cart */
     public function testRemoveFromCart()
     {
         $client = static::createClient();
